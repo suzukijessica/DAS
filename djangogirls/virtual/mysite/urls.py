295f90ac47a3b2459e391f715cacf2fb5c9699d5
Jessica Suzuki <suzuki_jessica@hotmail.com>
2016-07-01 15:58:48 -0300

    Adding login into the blog

--- djangogirls/virtual/mysite/urls.py
+++ djangogirls/virtual/mysite/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+import django.contrib.auth.views
+
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^accounts/login/$', django.contrib.auth.views.login, name='login'),
+    url(r'^accounts/logout/$', django.contrib.auth.views.logout, name='logout', kwargs={'next_page': '/'}),
     url(r'', include('blog.urls')),
 ]
